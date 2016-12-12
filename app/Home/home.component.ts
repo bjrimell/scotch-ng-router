979a3860a3aa0893e31ef746e3156a549138a559
bjrimell <barry@Barrys-MacBook-Air.local>
2016-12-12 23:00:37 +0000

    Added extra items to home page

--- app/Home/home.component.ts
+++ app/Home/home.component.ts
@@ -7,18 +7,7 @@ import { Observable } from 'rxjs/Observable';
 
 
 @Component({
-  template: `
-    <h1>Find your perfect route</h1>
-    <div class="form-group">
-      <label for="origin">From:</label>
-      <input type="text" class="form-control" [(ngModel)]="origin" id="origin" placeholder="Enter journey origin">
-    </div>
-    <div class="form-group">
-      <label for="destination">To:</label>
-      <input type="text" class="form-control inline" [(ngModel)]="destination" id="destination" placeholder="Enter journey destination">
-    </div>
-    <div class="text-center"><a [routerLink]="['/journey', 'how-to-get-from', origin, 'to', destination]" class="btn btn-primary btn-lg" role="button">Search Journeys</a></div>
-    `
+  templateUrl: `./app/Home/home.component.html`
 })
 // Component class
 export class HomeComponent implements OnInit {
